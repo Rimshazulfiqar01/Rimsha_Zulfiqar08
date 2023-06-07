cc56293dfd62a6aa234f2bb42cddb8a3aaab7443
Rimsha <rimshazulfiqar91@gmail.com>
2023-06-07 15:52:40 +0800

    made routes

--- ASSIGNMNETS/Assignment-3/mantra/server.js
+++ ASSIGNMNETS/Assignment-3/mantra/server.js
@@ -6,29 +6,37 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
-// Serve static files from the 'public' folder
 app.use(express.static('public'));
+// MongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/latestdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
-// // MongoDB connection
-// mongoose.connect('mongodb://127.0.0.1:27017/latestdb', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => {
-//     console.log('MongoDB connected');
-//   })
-//   .catch((err) => {
-//     console.error('MongoDB connection error:', err);
-//   });
+
+// Import the Ticket model
+const Ticket = require('./models/Ticket');
 
 // Define routes here
 const homeRouter = require('./routes/index');
+const addRouter = require('./routes/add');
+const deleteRouter = require('./routes/delete');
 const productsRouter = require('./routes/products');
 const contactRouter = require('./routes/contact');
+const displayRouter = require('./routes/display.js')
 
-app.use('/index', homeRouter);
+app.use('/', homeRouter);
 app.use('/products', productsRouter);
 app.use('/contact', contactRouter);
+app.use('/add',addRouter);
+app.use('/delete',deleteRouter);
+app.use('/display',displayRouter);
 
 
 app.listen(port, () => {
